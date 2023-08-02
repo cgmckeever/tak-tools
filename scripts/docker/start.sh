e2234f5336531a745d291e12d05be304b5786ff7
Chris McKeever <cgmckeever@rebelliondefense.com>
2023-08-01 21:14:27 -0500

    Creating docker install utils

--- scripts/docker/start.sh
+++ scripts/docker/start.sh
@@ -12,6 +12,8 @@ color success 92m
 color warning 93m
 color danger 91m
 
+CAPASS="atakatak"
+
 WORK_DIR=~/tak-server
 RELEASE_DIR="${WORK_DIR}/release"
 TAK_DIR="${RELEASE_DIR}/tak"
@@ -38,6 +40,8 @@ HOSTNAME=${HOSTNAME//\./-}
 read -p "What is the alias of this Tak Server [${HOSTNAME}]? " TAK_ALIAS
 TAK_ALIAS=${TAK_ALIAS:-$HOSTNAME}
 
+INTERMEDIARY_CA=${TAK_ALIAS}-Intermediate-CA
+
 echo; echo
 DEFAULT_NIC=$(route | grep default | awk '{print $8}')
 read -p "Which Network Interface [${DEFAULT_NIC}]? " NIC
@@ -45,12 +49,29 @@ NIC=${NIC:-${DEFAULT_NIC}}
 
 IP=$(ip addr show $NIC | grep -m 1 "inet " | awk '{print $2}' | cut -d "/" -f1)
 
+## Set variables for generating CA and client certs
+#
+printf $warning "SSL setup. Hit enter (x3) to accept the defaults:\n"
+read -p "State (for cert generation). Default [state] :" STATE
+export STATE=${STATE:-state}
+
+read -p "City (for cert generation). Default [city]:" CITY
+export CITY=${CITY:-city}
+
+read -p "Organization Name (for cert generation) [TAK]:" ORGANIZATION
+export ORGANIZATION=${ORGANIZATION:-TAK}
+
+read -p "Organizational Unit (for cert generation). Default [${ORGANIZATION}]:" ORGANIZATIONAL_UNIT
+export ORGANIZATIONAL_UNIT=${ORGANIZATIONAL_UNIT:-${ORGANIZATION}}
 
 ## CoreConfig
 #
 cp ${TEMPLATE_DIR}/CoreConfig-${VERSION}.xml.tmpl ${TAK_DIR}/CoreConfig.xml
-sed -i "s/PG_PASS/${PG_PASS}/" ${TAK_DIR}/CoreConfig.xml
-sed -i "s/HOSTIP/${IP}/g" ${TAK_DIR}/CoreConfig.xml
+sed -i "s/__PG_PASS/${PG_PASS}/" ${TAK_DIR}/CoreConfig.xml
+sed -i "s/__HOSTIP/${IP}/g" ${TAK_DIR}/CoreConfig.xml
+sed -i "s/__ORGANIZATION/${ORGANIZATION}/g" ${TAK_DIR}/CoreConfig.xml
+sed -i "s/__ORGANIZATIONAL_UNIT/${ORGANIZATIONAL_UNIT}/g" ${TAK_DIR}/CoreConfig.xml
+sed -i "s/__CAPASS/${CAPASS}/g" ${TAK_DIR}/CoreConfig.xml
 
 # Replaces takserver.jks with $IP.jks
 #sed -i "s/takserver.jks/$IP.jks/g" tak/CoreConfig.xml
@@ -62,17 +83,7 @@ sed -i "s/HOSTIP/${IP}/g" ${TAK_DIR}/CoreConfig.xml
 #
 sed -i "s/MemTotal/MemFree/g" ${TAK_DIR}/setenv.sh
 
-## Set variables for generating CA and client certs
-#
-printf $warning "SSL setup. Hit enter (x3) to accept the defaults:\n"
-read -p "State (for cert generation). Default [state] :" STATE
-export STATE=${STATE:-state}
 
-read -p "City (for cert generation). Default [city]:" CITY
-export CITY=${CITY:-city}
-
-read -p "Organizational Unit (for cert generation). Default [org]:" ORGANIZATIONAL_UNIT
-export ORGANIZATIONAL_UNIT=${ORGANIZATIONAL_UNIT:-orgunit}
 
 
 # Writes variables to a .env file for docker-compose
@@ -81,6 +92,7 @@ cat << EOF > ${RELEASE_DIR}/.env
 STATE=$STATE
 CITY=$CITY
 ORGANIZATIONAL_UNIT=$ORGANIZATIONAL_UNIT
+CAPASS=$CAPASS
 EOF
 
 cp ${TOOLS_DIR}/docker/compose.yml ${RELEASE_DIR}/
@@ -88,8 +100,6 @@ docker compose -f ${RELEASE_DIR}/compose.yml up --force-recreate -d
 
 ## Certs
 #
-INTERMEDIARY_CA=${TAK_ALIAS}-Intermediate-CA
-
 sleep 30
 
 while true;do
@@ -113,4 +123,5 @@ while true;do
     sleep 15
 done
 
+docker compose -f ${RELEASE_DIR}/compose.yml start tak-server
 
