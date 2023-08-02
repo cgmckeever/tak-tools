72774c27e6f4ac7d43d03100116076d0421ff341
Chris McKeever <cgmckeever@rebelliondefense.com>
2023-08-01 19:39:32 -0500

    Creating docker install utils

--- scripts/docker/prereq.sh
+++ scripts/docker/prereq.sh
@@ -21,7 +21,7 @@ sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-cache policy docker-ce
 sudo apt -y install docker-ce
-echo { "iptables" : false } >> /etc/docker/daemon.json
+echo '{ "iptables" : false }' >> /etc/docker/daemon.json
 
 sudo systemctl restart docker
 sudo systemctl enable docker
