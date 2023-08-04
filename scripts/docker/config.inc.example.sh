52d6fa2007b9a3a52c5a726e95711215996acd51
Chris McKeever <cgmckeever@rebelliondefense.com>
2023-08-04 09:43:26 -0500

    modular

--- scripts/docker/config.inc.example.sh
+++ scripts/docker/config.inc.example.sh
@@ -1,8 +1,8 @@
 ## Definable variables
 #  Copy this file to `config.inc.sh`
 #
-CAPASS="atakatak"               # CA Password
-CERTPASS="atakatak"             # User Cert Password
+CAPASS="atakatak"               # CA Password [not fully tested if a change works]
+CERTPASS="atakatak"             # User Cert Password [not fully tested if a change works]
 DOCKER_SUBNET="172.20.0.0/24"   # Docker subnet as defined in docker-compose.yml
-TAK_COT_PORT=8089               # TAK API Port
+TAK_COT_PORT=8089               # TAK API Port [not fully tested if a change works]
 TAKADMIN=tak-admin              # TAK Web Admin
