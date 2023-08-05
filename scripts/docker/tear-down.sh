7c8337a97331f06e32653dab047a47af96b1ffae
Chris McKeever <cgmckeever@rebelliondefense.com>
2023-08-05 09:36:23 -0500

    modular

--- scripts/docker/tear-down.sh
+++ scripts/docker/tear-down.sh
@@ -7,6 +7,8 @@ source ${SCRIPT_PATH}/shared.inc.sh
 
 ${DOCKER_COMPOSE} -f ~/tak-server/docker-compose.yml down
 docker volume rm --force tak-server_tak_data
+
+printf $warning "\n\nAnswer [y]es if you want to delete cotnainers.\n"
 docker system prune -a
 
 sudo systemctl disable tak-server-docker
