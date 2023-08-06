aab666c0aa4685c03af76ee8c862724d0d194316
Chris McKeever <cgmckeever@rebelliondefense.com>
2023-08-05 23:37:45 -0500

    add directory

--- scripts/tak/docker/setup.sh
+++ scripts/tak/docker/setup.sh
@@ -139,6 +139,9 @@ sed -i "s/__HOSTIP/${URL}/g" ${TAK_PATH}/CoreConfig.xml
 printf $info "Setting API Port: ${TAK_COT_PORT}\n\n"
 sed -i "s/__TAK_COT_PORT/${TAK_COT_PORT}/" ${TAK_PATH}/CoreConfig.xml
 
+printf $info "Setting PostGres Data URL: tak-database\n\n"
+sed -i "s/__PG_PASS/tak-database/" ${TAK_PATH}/CoreConfig.xml
+
 PG_PASS=${PAD2}$(pwgen -cvy1 -r ${PASS_OMIT} 25)${PAD1}
 printf $info "Setting PostGres Password: ${PG_PASS}\n\n"
 sed -i "s/__PG_PASS/${PG_PASS}/" ${TAK_PATH}/CoreConfig.xml
