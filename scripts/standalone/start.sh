e236d9efd1590c90092787c9af92c2abc7ded721
Chris McKeever <cgmckeever@rebelliondefense.com>
2023-08-08 04:28:50 -0500

    start standalone

---
+++ scripts/standalone/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SCRIPT_PATH=$(dirname "${BASH_SOURCE[0]}")
+source ${SCRIPT_PATH}/shared.inc.sh
+
+# =======================
+
+git config --global --add safe.directory '/opt/tak-tools'
+sudo git -C /opt/tak-tools pull
+
+${SCRIPT_PATH}/setup.sh
