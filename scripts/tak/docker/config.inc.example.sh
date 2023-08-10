7181693432b20a81f2692f42f3615c77f399973d
Chris McKeever <cgmckeever@rebelliondefense.com>
2023-08-10 09:58:18 -0500

    add DB connections to config

--- scripts/tak/docker/config.inc.example.sh
+++ scripts/tak/docker/config.inc.example.sh
@@ -4,7 +4,7 @@
 CAPASS="atakatak"               # CA Password [not fully tested if a change works]
 CERTPASS="atakatak"             # User Cert Password [not fully tested if a change works]
 DATABASE_ALIAS="tak-database"   # Docker network alias
-DATABASE_CONNECTIONS=500        # Number of concurrent database connections
+DATABASE_CONNECTIONS=2100       # Number of concurrent database connections
 DOCKER_SUBNET="172.20.0.0/24"   # Docker subnet
 TAK_COT_PORT=8089               # TAK API Port [not fully tested if a change works]
 TAKADMIN=tak-admin              # TAK Web Admin
--- scripts/tak/standalone/config.inc.example.sh
+++ scripts/tak/standalone/config.inc.example.sh
@@ -3,6 +3,6 @@
 #
 CAPASS="atakatak"               # CA Password [not fully tested if a change works]
 CERTPASS="atakatak"             # User Cert Password [not fully tested if a change works]
-DATABASE_CONNECTIONS=500        # Number of concurrent database connections
+DATABASE_CONNECTIONS=2100       # Number of concurrent database connections
 TAK_COT_PORT=8089               # TAK API Port [not fully tested if a change works]
 TAKADMIN=tak-admin              # TAK Web Admin
