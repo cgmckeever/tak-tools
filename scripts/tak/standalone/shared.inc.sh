02412fbf41b50496a64ed44d90cc5fe03831b0df
Chris McKeever <cgmckeever@rebelliondefense.com>
2023-08-11 22:43:48 -0500

    fix docker restart

--- scripts/tak/standalone/shared.inc.sh
+++ scripts/tak/standalone/shared.inc.sh
@@ -2,8 +2,14 @@
 
 PACKAGE_PATH="/tmp"
 
+SCRIPT_PATH="$(cd ${SCRIPT_PATH} && pwd)"
+echo $SCRIPT_PATH
+exit
+
 TOOLS_PATH=$(dirname $(dirname $(dirname $SCRIPT_PATH)))
 source ${TOOLS_PATH}/scripts/shared/functions.inc.sh
+
+TAK_SCRIPTS=${TOOLS_PATH}/scripts/tak/standalone
 
 TAK_PATH="/opt/tak"
 source ${TOOLS_PATH}/scripts/tak/shared/vars.inc.sh
