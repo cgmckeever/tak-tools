cafc966afe191806062091586fb24a9ff48af0d1
Chris McKeever <cgmckeever@rebelliondefense.com>
2023-08-12 17:28:18 -0500

    fix WORK_DIR

--- scripts/tak/docker/client-data-package.sh
+++ scripts/tak/docker/client-data-package.sh
@@ -5,12 +5,12 @@ source ${SCRIPT_PATH}/shared.inc.sh
 
 # =======================
 
-TAK_ALIAS=$($DOCKER_COMPOSE -f ${WORK_DIR}/docker-compose.yml exec tak-server bash -c "echo \$TAK_ALIAS" | tr -d '\r')
-URL=$($DOCKER_COMPOSE -f ${WORK_DIR}/docker-compose.yml exec tak-server bash -c "echo \$URL" | tr -d '\r')
-CAPASS=$($DOCKER_COMPOSE -f ${WORK_DIR}/docker-compose.yml exec tak-server bash -c "echo \$CAPASS" | tr -d '\r')
-PASS=$($DOCKER_COMPOSE -f ${WORK_DIR}/docker-compose.yml exec tak-server bash -c "echo \$PASS" | tr -d '\r')
-TAK_COT_PORT=$($DOCKER_COMPOSE -f ${WORK_DIR}/docker-compose.yml exec tak-server bash -c "echo \$TAK_COT_PORT" | tr -d '\r')
-TAK_CA=$($DOCKER_COMPOSE -f ${WORK_DIR}/docker-compose.yml exec tak-server bash -c "echo \$TAK_CA" | tr -d '\r')
+TAK_ALIAS=$($DOCKER_COMPOSE -f ${WORK_PATH}/docker-compose.yml exec tak-server bash -c "echo \$TAK_ALIAS" | tr -d '\r')
+URL=$($DOCKER_COMPOSE -f ${WORK_PATH}/docker-compose.yml exec tak-server bash -c "echo \$URL" | tr -d '\r')
+CAPASS=$($DOCKER_COMPOSE -f ${WORK_PATH}/docker-compose.yml exec tak-server bash -c "echo \$CAPASS" | tr -d '\r')
+PASS=$($DOCKER_COMPOSE -f ${WORK_PATH}/docker-compose.yml exec tak-server bash -c "echo \$PASS" | tr -d '\r')
+TAK_COT_PORT=$($DOCKER_COMPOSE -f ${WORK_PATH}/docker-compose.yml exec tak-server bash -c "echo \$TAK_COT_PORT" | tr -d '\r')
+TAK_CA=$($DOCKER_COMPOSE -f ${WORK_PATH}/docker-compose.yml exec tak-server bash -c "echo \$TAK_CA" | tr -d '\r')
 
 ## create package
 #
