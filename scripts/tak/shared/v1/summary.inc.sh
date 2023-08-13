1aa9e0bb54372833eb6b8ce2481215364f76a752
Chris McKeever <cgmckeever@rebelliondefense.com>
2023-08-13 04:47:29 -0500

    fix cert run as non-root

--- scripts/tak/shared/v1/summary.inc.sh
+++ scripts/tak/shared/v1/summary.inc.sh
@@ -13,14 +13,14 @@ printf $success "Login at https://$URL:8446 with your admin account user/pass.\n
 printf $success "No need to run the /setup step as this has been done.\n\n"
 
 INFO=${WORK_PATH}/info.txt
-echo "---------PASSWORDS----------------" > ${INFO}
-echo >> ${INFO}
-echo "Tak Admin user      : $TAKADMIN" >> ${INFO}
-echo "Tak Admin password  : $TAKADMIN_PASS" >> ${INFO}
-echo "PostgreSQL password : $PG_PASS" >> ${INFO}
-echo >> ${INFO}
-echo "---------PASSWORDS----------------" >> ${INFO}
-printf $danger "$(cat ${INFO})"
+sudo echo "---------PASSWORDS----------------" > ${INFO}
+sudo echo >> ${INFO}
+sudo echo "Tak Admin user      : $TAKADMIN" >> ${INFO}
+sudo echo "Tak Admin password  : $TAKADMIN_PASS" >> ${INFO}
+sudo echo "PostgreSQL password : $PG_PASS" >> ${INFO}
+sudo echo >> ${INFO}
+sudo echo "---------PASSWORDS----------------" >> ${INFO}
+printf $danger "$(sudo cat ${INFO})"
 
 printf $warning "\nMAKE A NOTE OF YOUR PASSWORDS. THEY WON'T BE SHOWN AGAIN.\n\n
 "
