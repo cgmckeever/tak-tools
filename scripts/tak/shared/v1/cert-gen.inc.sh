c14e906a844a2f381d2cca8b6826a52f0dc3f2b0
Chris McKeever <cgmckeever@rebelliondefense.com>
2023-08-13 04:05:26 -0500

    fix cert run as non-root

--- scripts/tak/shared/v1/cert-gen.inc.sh
+++ scripts/tak/shared/v1/cert-gen.inc.sh
@@ -25,17 +25,17 @@ echo "unique_subject=no" > files/crl_index.txt.attr
 while true;do
     printf $info "\n\n------------ Generating Certificates --------------"
     printf $success "\n\nRoot: ${TAK_ALIAS}-Root-CA-01\n"
-    ./makeRootCa.sh --ca-name $root {TAK_ALIAS}-Root-CA-01
+    sudo ./makeRootCa.sh --ca-name $root {TAK_ALIAS}-Root-CA-01
     if [ $? -eq 0 ];then
         TAK_CA=${TAK_ALIAS}-Intermediary-CA-01
         printf $success "\n\nCA: ${TAK_CA}\n"
-        ./makeCert.sh ca ${TAK_CA}
+        sudo ./makeCert.sh ca ${TAK_CA}
         if [ $? -eq 0 ];then
             printf $success "\n\nServer: ${TAK_ALIAS}\n"
-            ./makeCert.sh server ${TAK_ALIAS}
+            sudo ./makeCert.sh server ${TAK_ALIAS}
             if [ $? -eq 0 ];then
                 printf $success "\n\nAdmin: ${TAKADMIN}\n"
-                ./makeCert.sh client ${TAKADMIN}
+                sudo ./makeCert.sh client ${TAKADMIN}
                 if [ $? -eq 0 ];then
                     break
                 fi
