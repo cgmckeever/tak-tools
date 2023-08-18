c724c6e3c3692445a54691bb93c171e87aeafeda
Chris McKeever <cgmckeever@rebelliondefense.com>
2023-08-18 16:08:31 -0500

    add autoenroll handling

--- scripts/tak/docker/setup.sh
+++ scripts/tak/docker/setup.sh
@@ -117,6 +117,8 @@ while true;do
     sleep 10
 done
 
+source ${TAK_SCRIPT_PATH}/v1/autoenroll-data-package.inc.sh
+
 printf $success "\n\n ----------------- Installation Complete -----------------\n\n"
 
 ## Installation Summary
