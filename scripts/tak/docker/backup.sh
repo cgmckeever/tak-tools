15fffdfc40c646a88e84136a596d7822f6f96a89
Chris McKeever <cgmckeever@rebelliondefense.com>
2023-08-19 04:53:54 -0500

    backup certs
    exec
    
    exec
    
    exec
    
    exec
    
    exec
    
    exec
    
    exec
    
    exec
    
    exec
    
    exec
    
    exec
    
    exec
    
    exec
    
    exec
    
    exec
    
    exec
    
    exec

---
+++ scripts/tak/docker/backup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SCRIPT_PATH=$(dirname "${BASH_SOURCE[0]}")
+source ${SCRIPT_PATH}/shared.inc.sh
+
+# =======================
+
+source ${SCRIPT_PATH}/env.inc.sh
+source ${TAK_SCRIPT_PATH}/v1/backup.inc.sh $1
+
