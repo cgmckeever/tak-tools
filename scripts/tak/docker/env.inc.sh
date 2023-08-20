ddaeddea9a2bd1fabd01f9bdf9d816dcaadfb015
Chris McKeever <cgmckeever@rebelliondefense.com>
2023-08-20 03:08:52 -0500

    upgrade script

--- scripts/tak/docker/env.inc.sh
+++ scripts/tak/docker/env.inc.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-export TAK_ALIAS=$($DOCKER_COMPOSE -f ${DOCKER_COMPOSE_YML} exec tak-server bash -c "echo \$TAK_ALIAS" | tr -d '\r')
+export IP=$($DOCKER_COMPOSE -f ${DOCKER_COMPOSE_YML} exec tak-server bash -c "echo \$IP" | tr -d '\r')
 export URL=$($DOCKER_COMPOSE -f ${DOCKER_COMPOSE_YML} exec tak-server bash -c "echo \$URL" | tr -d '\r')
+
 export CAPASS=$($DOCKER_COMPOSE -f ${DOCKER_COMPOSE_YML} exec tak-server bash -c "echo \$CAPASS" | tr -d '\r')
 export PASS=$($DOCKER_COMPOSE -f ${DOCKER_COMPOSE_YML} exec tak-server bash -c "echo \$PASS" | tr -d '\r')
+
+export TAK_ALIAS=$($DOCKER_COMPOSE -f ${DOCKER_COMPOSE_YML} exec tak-server bash -c "echo \$TAK_ALIAS" | tr -d '\r')
 export TAK_COT_PORT=$($DOCKER_COMPOSE -f ${DOCKER_COMPOSE_YML} exec tak-server bash -c "echo \$TAK_COT_PORT" | tr -d '\r')
 export TAK_CA=$($DOCKER_COMPOSE -f ${DOCKER_COMPOSE_YML} exec tak-server bash -c "echo \$TAK_CA" | tr -d '\r')
 
---
+++ scripts/tak/docker/upgrade.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+SCRIPT_PATH=$(dirname "${BASH_SOURCE[0]}")
+source ${SCRIPT_PATH}/shared.inc.sh
+
+# =======================
+
+source ${SCRIPT_PATH}/env.inc.sh
+
+printf $warning "\n\nAvailable Releases: \n\n"
+ln -s release/*.zip
+
+echo; echo
+UPGRADE=""
+while ${UPGRADE} == ""; do
+    read -p "Which upgrade package: " UPGRADE
+done
+
+unzip release/${UPGRADE} -d release/
+
+printf $info "\n\nCreating Backup \n\n"
+source ${SCRIPT_PATH}/backup.sh n
+
+printf $info "Stopping Current Containers \n\n"
+docker-compose -f core-files/docker-compose.yml stop
+
+printf $info "Unlinking current release \n\n"
+rm -rf tak-server
+
+printf $info "Linking new release \n\n"
+RELEASE=${UPGRADE/".zip"/""}
+ln -s release/${RELEASE} tak-server
+
+printf $info "Copying configurations \n\n"
+cp ${BACKUP_PATH}/* tak-server/tak
+cp ${CORE_FILES}/.env tak-server/.env
+
+cat tak-server/tak/CoreConfig.xml
+cat tak-server/tak/UserAuthenticationFile.xml
+
+printf $info "Copying certs \n\n"
+mkdir -p tak-server/tak/certs/files/
+cp -R ${BACKUP_PATH}/cert-files/* tak-server/tak/certs/files/
+ls -la tak-server/tak/certs/files/
+
+printf $info "Restarting Server \n\n"
+docker-compose -f core-files/docker-compose.yml up  --build  -d
+
+echo
+source ${TAK_SCRIPT_PATH}/v1/server-check.inc.sh
