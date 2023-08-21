53d73bc63bcebdf07440e845512b18beb4f1e44e
Chris McKeever <cgmckeever@rebelliondefense.com>
2023-08-21 12:14:32 -0500

    network manager

--- scripts/tak/shared/v1/inputs.inc.sh
+++ scripts/tak/shared/v1/inputs.inc.sh
@@ -22,6 +22,7 @@ IP=$(ip addr show $NIC | grep -m 1 "inet " | awk '{print $2}' | cut -d "/" -f1)
 URL=$IP
 
 echo; echo
+printf $warning "Answering [y]es to the next prompt with restrict access to a VPN network."
 read -p "Is the TAK Server behind a VPN [Y/n]? " VPN
 VPN=${VPN:-y}
 
