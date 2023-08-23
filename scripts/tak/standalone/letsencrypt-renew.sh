cffc8aa9fcc18ee2b79af15fe829e11c144c4c93
Chris McKeever <cgmckeever@rebelliondefense.com>
2023-08-23 11:25:38 -0500

    firewall script

--- scripts/tak/standalone/letsencrypt-renew.sh
+++ scripts/tak/standalone/letsencrypt-renew.sh
@@ -13,4 +13,5 @@ source ${TOOLS_PATH}/scripts/shared/letsencrypt.inc.sh
 BACKUP_NAME=$(date +"%Y.%m.%d-%H.%M.%S")
 sudo cp ${FILE_PATH}/letsencrypt.jks ${FILE_PATH}/letsencrypt.${BACKUP_NAME}jks
 sudo cp ${FILE_PATH}/letsencrypt.p12 ${FILE_PATH}/letsencrypt.${BACKUP_NAME}p12
+sudo rm cp ${FILE_PATH}/letsencrypt*
 source ${TOOLS_PATH}/scripts/tak/shared/v1/letsencrypt-import.inc.sh
