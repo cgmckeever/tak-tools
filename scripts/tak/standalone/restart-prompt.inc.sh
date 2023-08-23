45b01a2d8290aa82e1f6cb669a3fe66b4cc7f76a
Chris McKeever <cgmckeever@rebelliondefense.com>
2023-08-23 11:49:27 -0500

    firewall script

--- scripts/tak/standalone/restart-prompt.inc.sh
+++ scripts/tak/standalone/restart-prompt.inc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-printf $warning "TAK needs to restart to enable changes.\n\n"
+printf $warning "\n\nTAK needs to restart to enable changes.\n\n"
 read -p "Restart TAK [y/n]? " RESTART
 
 if [[ $RESTART =~ ^[Yy]$ ]];then
