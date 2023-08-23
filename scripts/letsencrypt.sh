8a71f6cf095b71080de00cd4670338f630b54fa7
Chris McKeever <cgmckeever@rebelliondefense.com>
2023-08-23 10:45:28 -0500

    firewall script

--- scripts/letsencrypt.sh
+++ scripts/letsencrypt.sh
@@ -2,7 +2,6 @@
 
 SCRIPT_PATH="$(dirname "$(readlink -f "$0")")"
 TOOLS_PATH=$(dirname $SCRIPT_PATH)
-echo $TOOLS_PATH
 source ${TOOLS_PATH}/scripts/shared/functions.inc.sh
 
 # install certbot
@@ -11,34 +10,6 @@ sudo apt install -y certbot
 
 echo; echo
 printf $warning "Requesting a new certificate...\n\n"
+source ${TOOLS_PATH}/scripts/shared/letsencrypt.inc.sh
 
-read -p  "What is your domain name? [ex: atakhq.com or tak.foo.com] " FQDN
-
-echo
-read -p "What is your email? [Needed for LetsEncrypt Alerts] : " EMAIL
-
-echo
-read -p "Validate using [w]eb (must have port 80 exposed) or [d]ns: " VALIDATOR
-VALIDATOR=${VALIDATOR:-w}
-
-echo
-CERT="failed"
-case $(VALIDATOR) in
-  "d")
-    if sudo certbot certonly --manual --preferred-challenges dns -d ${FQDN} -m ${EMAIL}; then
-      CERT="issued"
-    fi
-  *)
-    if sudo certbot certonly --standalone -d ${FQDN} -m ${EMAIL} --agree-tos --non-interactive; then
-      CERT="issued"
-    fi
-    ;;
-esac
-
-if [[ "${CERT}" == "issued" ]]; then
-  printf $success "Certificate obtained successfully!\n\n"
-  echo $FQDN > ~/letsencrypt.txt
-else
-  printf $warning "Error obtaining certificate: $(sudo certbot certificates)"
-fi
 
