8655ac13f15c22057057a4cb946dfd39d1f4eb93
Chris McKeever <cgmckeever@rebelliondefense.com>
2023-08-23 09:51:52 -0500

    firewall script

--- scripts/tak/shared/v1/firewall.inc.sh
+++ scripts/tak/shared/v1/firewall.inc.sh
@@ -3,5 +3,5 @@
 printf $warning "\n\nThis is a simple/universal firewall configuration script for TAK.\n"
 printf $warning "You should verify that its not making unanticipated changes.\n"
 
-source ${TAK_SCRIPT_PATH}/v1/firewall.inc.sh
+source ${TAK_SCRIPT_PATH}/v1/firewall-update.inc.sh
 sudo ufw status numbered
