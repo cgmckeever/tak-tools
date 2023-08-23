dc62bbf6800f972c6e280f2cf6b9808594d88c97
Chris McKeever <cgmckeever@rebelliondefense.com>
2023-08-23 11:21:59 -0500

    firewall script

--- scripts/tak/docker/letsencrypt-renew.sh
+++ scripts/tak/docker/letsencrypt-renew.sh
@@ -10,5 +10,7 @@ source ${SCRIPT_PATH}/env.inc.sh
 printf $warning "\n\nRequesting a certificate renewal...\n\n"
 source ${TOOLS_PATH}/scripts/shared/letsencrypt.inc.sh
 
-sudo rm -rf ${FILE_PATH}/letsencrypt*
+BACKUP_NAME=$(date +"%Y.%m.%d-%H.%M.%S")
+cp ${FILE_PATH}/letsencrypt.jks ${FILE_PATH}/letsencrypt.${BACKUP_NAME}jks
+cp ${FILE_PATH}/letsencrypt.p12 ${FILE_PATH}/letsencrypt.${BACKUP_NAME}p12
 source ${TOOLS_PATH}/scripts/tak/shared/v1/letsencrypt-import.inc.sh
