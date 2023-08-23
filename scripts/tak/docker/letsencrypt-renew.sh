d6183eecbb08fe95e74eec97c98f7da599170400
Chris McKeever <cgmckeever@rebelliondefense.com>
2023-08-23 11:31:11 -0500

    firewall script

--- scripts/tak/docker/letsencrypt-renew.sh
+++ scripts/tak/docker/letsencrypt-renew.sh
@@ -7,11 +7,4 @@ source ${SCRIPT_PATH}/shared.inc.sh
 
 source ${SCRIPT_PATH}/env.inc.sh
 
-printf $warning "\n\nRequesting a certificate renewal...\n\n"
-source ${TOOLS_PATH}/scripts/shared/letsencrypt.inc.sh
-
-BACKUP_NAME=$(date +"%Y.%m.%d-%H.%M.%S")
-sudo cp ${FILE_PATH}/letsencrypt.jks ${FILE_PATH}/letsencrypt.${BACKUP_NAME}jks
-sudo cp ${FILE_PATH}/letsencrypt.p12 ${FILE_PATH}/letsencrypt.${BACKUP_NAME}p12
-sudo rm cp ${FILE_PATH}/letsencrypt*
-source ${TOOLS_PATH}/scripts/tak/shared/v1/letsencrypt-import.inc.sh
+source ${TAK_SCRIPT_PATH}/v1/letsencrypt-renew.inc.sh
---
+++ scripts/tak/shared/v1/letsencrypt-renew.inc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+printf $warning "\n\nRequesting a certificate renewal...\n\n"
+FQDN=$(cat ~/letsencrypt.txt)
+source ${TOOLS_PATH}/scripts/shared/letsencrypt.inc.sh ${FQDN}
+
+BACKUP_NAME=$(date +"%Y.%m.%d-%H.%M.%S")
+sudo cp ${FILE_PATH}/letsencrypt.jks ${FILE_PATH}/letsencrypt.${BACKUP_NAME}jks
+sudo cp ${FILE_PATH}/letsencrypt.p12 ${FILE_PATH}/letsencrypt.${BACKUP_NAME}p12
+sudo rm cp ${FILE_PATH}/letsencrypt*
+
+source ${TOOLS_PATH}/scripts/tak/shared/v1/letsencrypt-import.inc.sh
