789ca00882613200edc119e7537293e5209999c9
Chris McKeever <cgmckeever@rebelliondefense.com>
2023-08-23 11:09:34 -0500

    firewall script

--- scripts/tak/standalone/letsencrypt-renew.sh
+++ scripts/tak/standalone/letsencrypt-renew.sh
@@ -8,4 +8,4 @@ source ${SCRIPT_PATH}/shared.inc.sh
 printf $warning "\n\nRequesting a certificate renewal...\n\n"
 source ${TOOLS_PATH}/scripts/shared/letsencrypt.inc.sh
 
-source ${TOOLS_PATH}/scripts/tak/v1/letsencrypt-import.inc.sh
+source ${TOOLS_PATH}/scripts/tak/shared/v1/letsencrypt-import.inc.sh
