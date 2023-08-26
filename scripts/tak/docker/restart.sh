dc840ea04e2f54dcc96baf914bb0e101ce98af4a
Chris McKeever <cgmckeever@rebelliondefense.com>
2023-08-26 01:07:09 -0500

    restart script

--- scripts/tak/docker/restart.sh
+++ scripts/tak/docker/restart.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+SCRIPT_PATH=$(dirname "${BASH_SOURCE[0]}")
+source ${SCRIPT_PATH}/shared.inc.sh
+
+# =======================
+
+source ${SCRIPT_PATH}/env.inc.sh
+
 $DOCKER_COMPOSE -f ${DOCKER_COMPOSE_YML} restart tak-server
--- scripts/tak/standalone/restart.sh
+++ scripts/tak/standalone/restart.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-SCRIPT_PATH=$(dirname "${BASH_SOURCE[0]}")
-source ${SCRIPT_PATH}/shared.inc.sh
-
-# =======================
-
 sudo systemctl restart takserver
