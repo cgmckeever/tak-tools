3297ebb061e493671540838a70d13bcbb3d2a950
cgmckeever <cgmckeever@ironhide.local>
2025-05-23 13:13:49 -0500

    break out configure; start self-install work

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -16,13 +16,38 @@ install_init
 #
 source scripts/inc/package.sh
 
+RELEASE_DIR="${ROOT_PATH}/release"
+
+# Find folders (not files) under the release directory
+RELEASE=($(find "$RELEASE_DIR" -mindepth 1 -maxdepth 1 -type d | sort))
+
+if [ ${#RELEASE[@]} -eq 0 ]; then
+    msg $warn "No releases found under ${RELEASE_DIR}."
+    msg $warn "Please run the configure script to set up the environment."
+    exit 1
+fi
+
+# Prompt the user
+msg $warn "Select a release:"
+for i in "${!RELEASE[@]}"; do
+    msg $info "$((i+1)). ${RELEASE[$i]##*/}"
+done
+read -p "Enter a number [1-${#RELEASE[@]}]: " choice
+
+if [[ "$choice" =~ ^[0-9]+$ ]] && [ "$choice" -ge 1 ] && [ "$choice" -le "${#RELEASE[@]}" ]; then
+    TAK_ALIAS="${RELEASE[$((choice-1))]}"
+    msg $warn "${TAK_ALIAS} selected"
+else
+    msg $warn "Invalid selection. Exiting."
+    exit 1
+fi
+
+RELEASE_PATH=${ROOT_PATH}/release/${TAK_ALIAS}
+conf ${TAK_ALIAS}
+
 ## Tear-Down/Clean-up
 #
 scripts/${INSTALLER}/tear-down.sh ${TAK_ALIAS}
-
-## Generate Config
-#
-source ${SCRIPT_PATH}/gen-conf.sh
 
 info ${RELEASE_PATH} "---- TAK Info: ${TAK_ALIAS} ----" init
 info ${RELEASE_PATH} "Install: ${INSTALLER}"
@@ -37,7 +62,6 @@ info ${RELEASE_PATH} "  User: martiuser"
 info ${RELEASE_PATH} "  Password: ${TAK_DB_PASS}" 
 info ${RELEASE_PATH} ""
 
-conf ${TAK_ALIAS}
 letsencrypt
 
 ## Configure
